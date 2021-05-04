d8c33c1f0fac55756a7f9c5b5b9269b1094b70f4
Leo June Bedeo <jfljfb@gmail.com>
2021-05-04 22:55:37 +0800

    Update form.blade.php

--- resources/views/pages/vehicle-types/form.blade.php
+++ resources/views/pages/vehicle-types/form.blade.php
@@ -26,7 +26,7 @@
                             <div class="px-4 py-5 bg-white sm:p-6">
                                 <div class="grid grid-cols-6 gap-6">
                                     <div class="col-span-6 sm:col-span-3">
-                                        <label for="first_name" class="block text-sm font-medium text-gray-700">Vehicle Type</label>
+                                        <label for="vehicle_type" class="block text-sm font-medium text-gray-700">Vehicle Type</label>
                                         <input type="text" name="vehicle_type" id="vehicle_type" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" value="{{ isset($vehicle_type) ? $vehicle_type->vehicle_type : "" }}">
                                     </div>
                                 </div>
