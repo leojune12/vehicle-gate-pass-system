6aa0b22dc14fdf098f2e3a6e10d9f02d466d24ae
Leo June Bedeo <leojune.bedeo@gmail.com>
2021-05-04 10:50:55 +0800

    Create migrations

---
+++ database/migrations/2021_05_04_020207_create_drivers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->string('rfid_id', 50);
+            $table->string('name', 255);
+            $table->string('address', 255);
+            $table->string('contact_number', 255);
+            $table->unsignedBigInteger('vehicle_type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+}
