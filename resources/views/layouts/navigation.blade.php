055cb87228e4576ca9c036d586bb368dc3824dbc
Leo June Bedeo <jfljfb@gmail.com>
2021-05-04 23:20:03 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,11 @@
                         {{ __('Vehicle Types') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="/driver-types" :active="request()->routeIs('driver-types')">
+                        {{ __('Driver Types') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
