15d5d53f7a4ee528956877c7bd385e2623babeda
Leo June Bedeo <jfljfb@gmail.com>
2021-05-07 22:55:49 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,6 @@ use App\Http\Controllers\LogController;
 use App\Http\Controllers\LogTypeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VehicleTypeController;
-use App\Models\Driver;
-use App\Models\DriverType;
-use App\Models\Log;
-use App\Models\LogType;
-use App\Models\User;
-use App\Models\VehicleType;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,27 +18,27 @@ Route::middleware(['auth'])->group(function () {
 
     Route::resource('users', UserController::class);
 
-    Route::get('/users', [DashboardController::class, 'index'])->name('users');
+    Route::get('/users', [UserController::class, 'index'])->name('users');
 
     Route::resource('drivers', DriverController::class);
 
-    Route::get('/drivers', [DashboardController::class, 'index'])->name('drivers');
+    Route::get('/drivers', [DriverController::class, 'index'])->name('drivers');
 
     Route::resource('driver-types', DriverTypeController::class);
 
-    Route::get('/driver-types', [DashboardController::class, 'index'])->name('driver-types');
+    Route::get('/driver-types', [DriverTypeController::class, 'index'])->name('driver-types');
 
     Route::resource('vehicle-types', VehicleTypeController::class);
 
-    Route::get('/vehicle-types', [DashboardController::class, 'index'])->name('vehicle-types');
+    Route::get('/vehicle-types', [VehicleTypeController::class, 'index'])->name('vehicle-types');
 
     Route::resource('logs', LogController::class);
 
-    Route::get('/logs', [DashboardController::class, 'index'])->name('logs');
+    Route::get('/logs', [LogController::class, 'index'])->name('logs');
 
     Route::resource('log-types', LogTypeController::class);
 
-    Route::get('/log-types', [DashboardController::class, 'index'])->name('log-types');
+    Route::get('/log-types', [LogTypeController::class, 'index'])->name('log-types');
 });
 
 require __DIR__ . '/auth.php';
