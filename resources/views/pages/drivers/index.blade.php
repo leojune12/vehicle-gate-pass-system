9842836b4a87f3ef519559771b304b571f962ff1
Leo June <leojune.bedeo@pueblodepanay.com>
2021-05-07 09:25:13 +0800

    Update index.blade.php

--- resources/views/pages/drivers/index.blade.php
+++ resources/views/pages/drivers/index.blade.php
@@ -11,8 +11,8 @@
                 <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="mb-4 flex justify-end">
-                            <a href="/drivers/create" class="rounded bg-green-500 text-white px-4 py-2 hover:bg-green-600">
-                                Add Driver
+                            <a href="/drivers/create" class="rounded bg-green-500 text-white px-4 py-2 hover:bg-green-600 flex items-center">
+                                <span>Add Driver</span>
                             </a>
                         </div>
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -64,6 +64,8 @@
                                                 {{ $driver->driver_type->driver_type }}
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex">
+                                                <a href="/drivers/{{ $driver->id }}" class="text-indigo-600 hover:text-indigo-900">View Logs</a>
+                                                <div>&nbsp;|&nbsp;</div>
                                                 <a href="/drivers/{{ $driver->id }}/edit" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                                 <div>&nbsp;|&nbsp;</div>
                                                 <form action="/drivers/{{ $driver->id }}" method="POST">
