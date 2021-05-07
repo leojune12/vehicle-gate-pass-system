8b9931524ddaa889ce9d7f0e0f5ec9f9622bfd8b
Leo June Bedeo <jfljfb@gmail.com>
2021-05-07 22:56:10 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,8 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
+                <img src="{{ asset('media/capsu.png') }}" alt="logo" class="object-scale-down w-36 h-36">
             </a>
         </x-slot>
 
@@ -41,13 +42,13 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+                {{-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
-                @endif
+                @endif --}}
 
-                <x-button class="ml-3">
+                <x-button class="w-full bg-yellow-400 hover:bg-yellow-500 justify-center flex py-2">
                     {{ __('Log in') }}
                 </x-button>
             </div>
