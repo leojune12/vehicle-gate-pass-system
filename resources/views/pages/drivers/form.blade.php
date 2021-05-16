d1797fdc25e4d1c056078b1ef8d37fb7fcb161e5
Leo June Bedeo <jfljfb@gmail.com>
2021-05-16 10:49:40 +0800

    Add text to label

--- resources/views/pages/drivers/form.blade.php
+++ resources/views/pages/drivers/form.blade.php
@@ -14,7 +14,7 @@
                 <div class="md:grid md:grid-cols-3 md:gap-6">
                     <div class="md:col-span-1">
                         <div class="px-4 sm:px-0">
-                            <h3 class="text-lg font-medium leading-6 text-gray-900">Driver</h3>
+                            <h3 class="text-lg font-medium leading-6 text-gray-900">Driver Information</h3>
                         </div>
                     </div>
                     <div class="mt-5 md:mt-0 md:col-span-2">
@@ -142,14 +142,13 @@
                                     </div>
                                 </div>
                             </div>
-                                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                    <a href="/drivers" class="inline-flex justify-center py-2 px-4 border border-indigo-400 border-transparent shadow-sm text-sm font-medium rounded-md text-grey-600 bg-white hover:bg-indigo-100 focus:outline-none">
-                                        Cancel
-                                    </a>
-                                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        {{ isset($driver) ? "Update" : "Save" }}
-                                    </button>
-                                </div>
+                            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                <a href="/drivers" class="inline-flex justify-center py-2 px-4 border border-indigo-400 border-transparent shadow-sm text-sm font-medium rounded-md text-grey-600 bg-white hover:bg-indigo-100 focus:outline-none">
+                                    Cancel
+                                </a>
+                                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    {{ isset($driver) ? "Update" : "Save" }}
+                                </button>
                             </div>
                         </form>
                     </div>
